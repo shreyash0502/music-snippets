2b43dd56de3d55777a7d11ba5d3263c12cdbe493
shreyash2912 <56553419+shreyash2912@users.noreply.github.com>
2021-06-13 19:09:52 +0530

    Edit3

--- demo/settings.py
+++ demo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -52,10 +53,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'demo.urls'
 
-import dj_database_url
-        db_from_env = dj_database_url.config(conn_max_age=500)
-		DATABASES['default'].update(db_from_env)
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -84,6 +81,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
