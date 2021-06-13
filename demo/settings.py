e112072fcb100f97573c6706ea02ece0eeabb362
shreyash2912 <56553419+shreyash2912@users.noreply.github.com>
2021-06-13 19:20:58 +0530

    Edit4

--- demo/settings.py
+++ demo/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'demo.urls'
@@ -132,4 +133,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
