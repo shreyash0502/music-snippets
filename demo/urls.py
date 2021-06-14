2e8673228c58e7d2e188aff6fde9128d1a58ba36
shreyash2912 <56553419+shreyash2912@users.noreply.github.com>
2021-06-14 11:16:55 +0530

    edit

--- demo/urls.py
+++ demo/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     path('', views.index, name='homepage')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
